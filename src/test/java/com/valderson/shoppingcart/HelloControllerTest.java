18aa4a716cd802751b451f06871f57ae2a1caf06
valdersonjr <valdersopontes@gmail.com>
2025-05-10 09:59:35 -0300

    (test) Adiciona infraestrutura de testes

---
+++ src/test/java/com/valderson/shoppingcart/HelloControllerTest.java
@@ -0,0 +1,13 @@
+package com.valderson.shoppingcart;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class HelloControllerTest {
+
+    @Test
+    @Disabled("Ainda não implementado")
+    void testHelloEndpoint() {
+        // Teste desativado propositalmente
+    }
+}
