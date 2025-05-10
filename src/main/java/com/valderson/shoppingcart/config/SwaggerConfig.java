bac86fae7bee3cbc1d8b191806f47c6bb137c478
valdersonjr <valdersopontes@gmail.com>
2025-05-10 10:12:39 -0300

    (feat) Adiciona configuração do swagger

--- src/main/java/com/valderson/shoppingcart/config/SwaggerConfig.java
+++ src/main/java/com/valderson/shoppingcart/config/SwaggerConfig.java
@@ -1,14 +1,15 @@
-package br.com.gtamiozzo.gadernal.config;
+package com.valderson.shoppingcart.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.*;
-import io.swagger.v3.oas.models.security.*;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -17,25 +18,25 @@ public class SwaggerConfig {
     public OpenAPI gadernalApi() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Backoffice Integration API")
-                        .description("API para lidar com interação do administrador com a geladeira")
+                        .title("Shopping Cart")
+                        .description("")
                         .version("v1.0")
                         .contact(new Contact()
-                                .name("Tamiozzo")
-                                .email("gabriel.tamiozzo@gmail.com"))
+                                .name("Valderson")
+                                .email("valdersonpontes@gmail.com"))
                         .license(new License()
                                 .name("Apache 2.0")
                                 .url("http://springdoc.org"))
-                )
-                .components(new Components()
-                        .addSecuritySchemes("bearer-jwt",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")
-                        )
-                )
-                .addSecurityItem(new SecurityRequirement().addList("bearer-jwt"));
+                );
+//                .components(new Components()
+//                        .addSecuritySchemes("bearer-jwt",
+//                                new SecurityScheme()
+//                                        .type(SecurityScheme.Type.HTTP)
+//                                        .scheme("bearer")
+//                                        .bearerFormat("JWT")
+//                        )
+//                );
+//                .addSecurityItem(new SecurityRequirement().addList("bearer-jwt"));
     }
 
     @Bean
